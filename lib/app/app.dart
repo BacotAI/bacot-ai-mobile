c9158306bd575a165b00bdee66039aac5e396570
aldodevv <aldodevox@gmail.com>
2025-12-22 09:54:20 +0700

    feat: add logarte interceptor and fix config ml kit on ios

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,19 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:logarte/logarte.dart';
+import 'package:smart_interview_ai/app/di.dart';
 import 'package:smart_interview_ai/app/router/app_router.dart';
 import 'package:smart_interview_ai/core/config/app_theme.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   final bool isLoggedIn;
-  final _appRouter = AppRouter();
 
-  App({super.key, required this.isLoggedIn});
+  const App({super.key, required this.isLoggedIn});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  late final AppRouter _appRouter;
+  late final Logarte logarte;
+
+  @override
+  void initState() {
+    super.initState();
+    _appRouter = AppRouter();
+    logarte = sl<Logarte>();
+
+    logarte.attach(context: context, visible: false);
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
       title: 'Smart Interview AI',
       theme: AppTheme.light,
-      routerConfig: _appRouter.config(),
+      routerConfig: _appRouter.config(
+        navigatorObservers: () => [LogarteNavigatorObserver(logarte)],
+      ),
     );
   }
 }
